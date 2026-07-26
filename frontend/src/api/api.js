0377329965487de43f1f89d3f0d5919bc3d8e81c
Dinesh T <dineshkarthikklm92@gmail.com>
2026-07-26 07:24:09 +0530

    v8

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,21 +4,26 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/",
 });
 
-api.interceptors.request.use(
-  (config) => {
+api.interceptors.request.use((config) => {
+  const publicEndpoints = [
+    "auth/login/",
+    "auth/register/",
+    "auth/token/refresh/",
+  ];
 
-    const token =
-      localStorage.getItem(
-        "accessToken"
-      );
+  const isPublic = publicEndpoints.some((endpoint) =>
+    config.url?.includes(endpoint)
+  );
+
+  if (!isPublic) {
+    const token = localStorage.getItem("accessToken");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
-
-    return config;
   }
-);
+
+  return config;
+});
 
 export default api;
